1b03060da9f4361f67633799594c8da351e204f9
Hallahan <sean.hallahan@accenturefederal.com>
2018-02-20 17:50:01 -0500

    updated about us and contact pages

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
 let navOpen = false;
 
 function toggleNavigation() {
-  const navContainer = document.querySelector('.nav__container');
-  if (navOpen) {
-    navContainer.classList.remove('nav__container--open');
-    navContainer.classList.add('nav__container--closed');
-  } else {
-    navContainer.classList.remove('nav__container--closed');
-    navContainer.classList.add('nav__container--open');
-  }
-  return (navOpen = !navOpen);
+	const navContainer = document.querySelector(".nav__container");
+	if (navOpen) {
+		navContainer.classList.remove("nav__container--open");
+		navContainer.classList.add("nav__container--closed");
+	} else {
+		navContainer.classList.remove("nav__container--closed");
+		navContainer.classList.add("nav__container--open");
+	}
+	return (navOpen = !navOpen);
 }
 
-function startCarosel() {
-  let activeImage = 0;
-  const images = document.querySelectorAll('.carosel__container');
+function startcarousel() {
+	let activeImage = 0;
+	const images = document.querySelectorAll(".carousel__container");
+  if(images.length < 2) return
+	setInterval(() => {
+		images[activeImage].classList.remove("carousel__container--active");
 
-  setInterval(() => {
-    images[activeImage].classList.remove('carosel__container--active');
-
-    if (images.length === activeImage + 1) {
-      activeImage = 0;
-    } else {
-      activeImage++;
-    }
-    images[activeImage].classList.add('carosel__container--active');
-  }, 10000);
-  ß;
+		if (images.length === activeImage + 1) {
+			activeImage = 0;
+		} else {
+			activeImage++;
+		}
+		images[activeImage].classList.add("carousel__container--active");
+	}, 10000);
+	;
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const navButton = document.querySelector('.nav__button');
+window.addEventListener("DOMContentLoaded", () => {
+	const navButton = document.querySelector(".nav__button");
 
-  navButton.addEventListener('click', toggleNavigation);
+	navButton.addEventListener("click", toggleNavigation);
 });
 
-window.addEventListener('load', startCarosel);
+window.addEventListener("load", startcarousel);
